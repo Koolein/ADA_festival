01f558d3d5ce148c8475dd3948dca8e0323ee6dd
Koolein <146184868+Koolein@users.noreply.github.com>
2025-05-21 20:02:56 +0200

    Update User_dao.py

--- Festival_API/User_micro/daos/User_dao.py
+++ Festival_API/User_micro/daos/User_dao.py
@@ -1,28 +1,19 @@
 from sqlalchemy import Column, String, Integer, DateTime, Enum
 from sqlalchemy.orm import relationship
-from constant import FestivalStatus
 from db import Base
 import enum
+from datetime import datetime
 
-class FestivalDAO(Base):
-    __tablename__ = "festivals"
+class UserDAO(Base):
+    __tablename__ = "users"
     id          = Column(Integer, primary_key=True)
-    name        = Column(String, nullable=False)
-    start_date  = Column(DateTime)
-    end_date    = Column(DateTime)
-    sessions = relationship("SessionDAO", 
-                            back_populates="festival", 
-                            cascade="all, delete-orphan"
-                            )
-    status     = Column(Enum(FestivalStatus, name='Festival_status_enum'), nullable=False, default=FestivalStatus.SCHEDULED)
-    def __init__(self, id, name, start_date, end_date, status=FestivalStatus.SCHEDULED):
+    username    = Column(String, unique=True, nullable=False)
+    email       = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    
+    def __init__(self, id, username, email, password, role=UserRole.USER, status=UserStatus.ACTIVE):
         self.id = id
-        self.name = name
-        self.start_date = start_date
-        self.end_date = end_date
-        self.status = status
-        
-
-        
-
-   
+        self.username = username
+        self.email = email
+        self.password = password
+      
