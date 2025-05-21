02f980405bb7c3c96508405f42b62de666a83a09
Koolein <c.j.a.leppink@tilburguniversity.edu>
2025-05-21 15:28:45 +0200

    d

--- Festival_API/Festival_micro/daos/Session_dao.py
+++ Festival_API/Festival_micro/daos/Session_dao.py
@@ -14,11 +14,14 @@ class SessionDAO(Base):
     end_time    = Column(DateTime)
     status      = Column(Enum(SessionStatus, name="Session_status_enum"), nullable=False, default=SessionStatus.STATUS_UPCOMING)
     festival = relationship("FestivalDAO", back_populates="sessions")
-    
-    def __init__(self, status=SessionStatus.STATUS_UPCOMING, id=None, festival_id=None, title=None, start_time=None, end_time=None):
+    capacity    = Column(Integer, nullable=True)
+    issued      = Column(Integer, nullable=True)
+
+    def __init__(self, capacity, status=SessionStatus.STATUS_UPCOMING, id=None, festival_id=None, title=None, start_time=None, end_time=None):
         self.id = id
         self.festival_id = festival_id
         self.title = title
         self.start_time = start_time
         self.end_time = end_time
         self.status = status
+        self.capacity = capacity
