c8f0e026c5f84cd68c42b8f53afd1d020a3d92c1
Koolein <c.j.a.leppink@tilburguniversity.edu>
2025-05-21 22:39:37 +0200

    check

--- Festival_API/User_micro/db.py
+++ Festival_API/User_micro/db.py
@@ -1,27 +1,20 @@
 import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
 
-# read creds from env
-DB_USER = os.getenv("DB_USER", "api_user")
-DB_PASS = os.getenv("DB_PASS", "yourStrongP@ss")
-DB_NAME = os.getenv("DB_NAME", "userdb")
-DB_HOST = os.getenv("DB_HOST", "34.46.26.37")
-DB_PORT = int(os.getenv("DB_PORT", "5432"))
-
-# build engine without user:pass@host in the URL
-engine = create_engine(
-    "postgresql+psycopg2://",
-    connect_args={
-        "user": DB_USER,
-        "password": DB_PASS,
-        "host": DB_HOST,
-        "port": DB_PORT,
-        "database": DB_NAME,
-    },
-    echo=True,
-)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# The database URL is provided as an env. variable
+if 'DB_URL' in os.environ:
+    db_url = os.environ['DB_URL']
+else:
+    db_url = 'sqlite:///user.db'
+engine = create_engine(db_url)
+if not database_exists(engine.url):
+    create_database(engine.url)
+print(database_exists(engine.url))
+# https://docs.sqlalchemy.org/en/13/orm/session.html
+Session = sessionmaker(bind=engine)
+# https://docs.sqlalchemy.org/en/13/orm/extensions/declarative/api.html
 Base = declarative_base()
